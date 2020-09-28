7b020b38708c5ff1035e76ef25f6f6c6d7ce1bb6
ajay pandey <ajaypandey348@gmail.com>
2020-09-28 13:58:04 +0530

    Updates

---
+++ precache-manifest.3958fc3597e15a602e04b1d58cb4f332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc70e512360cea3ee42b5d616ba0897",
+    "url": "/http//AjayPandeyGit.github.io/react-UserList/index.html"
+  },
+  {
+    "revision": "4a5fc5f16ed190871951",
+    "url": "/http//AjayPandeyGit.github.io/react-UserList/static/css/main.0727bc9d.chunk.css"
+  },
+  {
+    "revision": "5526380ab317a6222491",
+    "url": "/http//AjayPandeyGit.github.io/react-UserList/static/js/2.87123d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//AjayPandeyGit.github.io/react-UserList/static/js/2.87123d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5fc5f16ed190871951",
+    "url": "/http//AjayPandeyGit.github.io/react-UserList/static/js/main.ac820caf.chunk.js"
+  },
+  {
+    "revision": "e66cdad986bd0705168c",
+    "url": "/http//AjayPandeyGit.github.io/react-UserList/static/js/runtime-main.0688c55b.js"
+  }
+]);
